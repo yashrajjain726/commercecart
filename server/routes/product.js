aadb3b82e0eb3c3021ef7b1402da182a467f31b2
Yashraj Jain <yashrajjain726@gmail.com>
2023-07-06 23:23:10 +0530

    deal api added

--- server/routes/product.js
+++ server/routes/product.js
@@ -49,4 +49,24 @@ productRouter.post("/api/product/rating", auth, async (request, response) => {
   }
 });
 
+productRouter.post("/api/products/deal", auth, async (request, response) => {
+  let products = await Product.find({});
+  products = products.sort((a, b) => {
+    let aSum = 0;
+    let bSum = 0;
+    for (let i = 0; i < a.ratings.length; i++) {
+      aSum += a.ratings[i].rating;
+    }
+    for (let i = 0; i < b.ratings.length; i++) {
+      bSum += b.ratings[i].rating;
+    }
+    return aSum < bSum ? 1 : -1;
+  });
+  response.json(products[0]);
+  try {
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = productRouter;
