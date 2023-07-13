a5783351933a508ff5fe9ba9eabe4a121e5f1557
Yashraj Jain <yashrajjain726@gmail.com>
2023-07-13 11:10:14 +0530

    separate backend

--- server/models/rating.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const ratingSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  rating: { type: Number, required: true },
-});
-module.exports = ratingSchema;
